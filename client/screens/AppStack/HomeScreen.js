5dfe1b758e317678a1a7f51b234e2f871f361837
Cenga387 <imocenga@gmail.com>
2024-04-02 19:47:00 +0200

    Home page almost done

--- client/screens/AppStack/HomeScreen.js
+++ client/screens/AppStack/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, ImageBackground, Dimensions, ScrollView, Pressable } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground, Dimensions, ScrollView, Pressable, TextInput } from 'react-native';
 import { SearchBar } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 
@@ -47,6 +47,7 @@ export default function HomeScreen () {
                         </View>
                     </View>
                     <View style={styles.content}>
+
                         <View style={styles.contentContainer1}>
                             <View style={styles.featuredArticles}>
                                 <Text style={styles.title2}>Featured articles</Text>
@@ -66,6 +67,7 @@ export default function HomeScreen () {
                                 <Text style={styles.title2}>Profile</Text>
                             </View>
                         </View>
+
                         <View style={styles.contentContainer2}>
                             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} snapToInterval={1200} decelerationRate={0} snapToAlignment="center">
                                 <View style={{justifyContent: 'space-between', flexDirection: 'row', width: '100%'}}>
@@ -79,6 +81,7 @@ export default function HomeScreen () {
                                 </View>
                             </ScrollView>
                         </View>
+
                         <View style={styles.contentContainer3}>
                             <Text style={{marginVertical: 16, fontSize: 35, color: 'white'}}>Fact of the day</Text>
                             <View style={styles.dailyFact}>
@@ -86,13 +89,89 @@ export default function HomeScreen () {
                                 <View style={styles.factImage}></View>
                             </View>
                         </View>
+
                         <View style={styles.contentContainer4}>
                             <Text style={{fontSize: 35, marginVertical: 16, color: 'white'}}>Fun in every category</Text>
-                            <View style={styles.columns}>
-
+                            <View style={styles.row1}>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>History</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>Science</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>Geography</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
+                            </View>
+                            <View style={styles.row2}>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>Nature & Wildlife</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>Arts & Culture</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.categories}>
+                                    <Text style={{fontSize: 35, color: 'white'}}>Biographies</Text>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                    <View style={styles.article4}>
+                                        <View style={styles.article4Image}></View>
+                                        <Text style={{fontSize: 17, color: 'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                                    </View>
+                                </View>
                             </View>
                         </View>
                     </View>
+                    <View style={styles.subscribe}>
+                            <Text style={{fontSize: 25, marginTop: 30, marginBottom: 15, color: 'white'}}>Sign up here to receive daily articles via email</Text>
+                            <View style={{flexDirection: 'row', width: '100%', justifyContent: 'center', marginVertical: 30}}>
+                                <TextInput style={styles.emailInput} placeholder="Enter your email here"></TextInput>
+                                <Pressable style={styles.subscribeButton}>Subscribe</Pressable>
+                            </View>
+                            <Text style={{fontSize: 20, marginVertical: 15, color: 'white', marginHorizontal: 40, textAlign: 'center'}}>By signing up for this email, you are agreeing to news, offers, and information from Universeum. Click here to view our Privacy Notice. Easy unsubscribe links are provided in every email.</Text>
+                        </View>
                 </ScrollView>
             </ImageBackground>
         </View>
@@ -164,11 +243,13 @@ const styles = StyleSheet.create({
     },
     contentContainer3: {
         marginTop: 40,
+        marginBottom: 100,
         height: 200,
     },
     contentContainer4: {
         marginTop: 40,
-        // paddingHorizontal: 70
+        width: '100%',
+        height: 1000,
     },
     featuredArticles: {
         width: '70%',
@@ -197,18 +278,63 @@ const styles = StyleSheet.create({
         opacity: 0.8,
         backgroundColor: '#330762'
     },
+    article4: {
+        flexDirection: 'row', 
+        marginVertical: 20, 
+        marginRight: 10
+    },
     dailyFact: {
         height: 250,
         flexDirection: 'row',
     },
     factImage: {
         width: 500,
-        height: '100%',
+        height: 200,
         backgroundColor: '#330762',
         marginLeft: '10%',
-
     },
-    columns: {
-        flexDirection: 'row'
+    row1: {
+        flexDirection: 'row',
+    },
+    row2: {
+        flexDirection: 'row',
+    },
+    categories: {
+        width: '33%',
+        height: 350,
+        backgroundColor: 'rgba(51, 7, 98, 0.75)',
+        marginHorizontal: 4,
+        marginVertical: 4
+    },
+    article4Image: {
+        width: 150, 
+        height: 100, 
+        backgroundColor: '#330762', 
+        marginRight: 10, 
+        marginTop: 7
+    },
+    subscribe: {
+        backgroundColor: '#330762',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    emailInput: {
+        height: 40,
+        backgroundColor: 'white',
+        width: '70%',
+        borderRadius: 10,
+        textAlign: 'center'
+    },
+    subscribeButton: {
+        height: 40,
+        width: 100,
+        marginLeft: 5,
+        fontSize: 20,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 10,
+        justifyContent: 'center',
+        color: 'white'
     }
 })
